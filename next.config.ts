6c478ab72c18ca35fbc47423e0ecc8b3a8b9d350
ElBrAyAn1967 <brayan002150@gmail.com>
2025-11-17 10:01:29 -0600

    primer cambio

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  // Configuración vacía de Turbopack para evitar errores
+  turbopack: {},
 };
 
 export default nextConfig;
